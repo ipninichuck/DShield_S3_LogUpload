2a834aff65c9d28ef9797a71c0e65b3fb15fee5d
ipninichuck <ipninichuck@gmail.com>
2018-11-02 21:00:20 -0700

    Update dshield_upload.py

--- dshield_upload.py
+++ dshield_upload.py
@@ -1,10 +1,14 @@
 import bota3
 import datetime
 
+#We will store the current timestamp for later use. Also the bucketname being used.
 now = datetime.datetime.now()
-objectname = 'DshieldLog_%s' % now
-logpath = 'var/log/dshield.log'
 bucketname = 'serveractivityuploads'
 
-s3 = boto3.resource('s3',)
-s3.Bucket(bucketname).upload_files(logpath, objectname)
+#We will make a dictionary to store Objectname:logpath key pairs
+d = 'DshieldLog_%s' % now: 'var/log/dshield.log',
+
+#For loop will then upload each item in the dictionary to the S3 Bucket
+for  k,v in d.items():
+  s3 = boto3.resource('s3',)
+  s3.Bucket(bucketname).upload_files(k, v)
