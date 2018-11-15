90df4469110d90f8334a5798f4cb82fbedc19ea9
ipninichuck <ipninichuck@gmail.com>
2018-11-15 00:06:32 -0800

    Add files via upload

---
+++ directoryuploader.py
@@ -0,0 +1,26 @@
+import boto3
+import datetime
+import os.path
+import sys
+
+#We will store the current timestamp for later use
+#We will also specify the bucket and source directory
+#We will also creat a device id variable
+
+now = datetime.datetime.now()
+bucketname = 'serveractivityuploads'
+sourceDir = 
+deviceid = 
+
+#We will make a list of the file names to be uploaded
+uploadFileNames = []
+for (sourceDir, dirname, filename) in os.walk(sourceDir):
+    uploadFileNames.extend(filename)
+    break
+
+#For loop goes through each file and uploads it to s3
+for filename in uploadFileNames:
+    sourcepath = os.path.join(sourceDir + '/' + filename) 
+    logname = deviceid + '_' + filename + '_' + now   
+    s3 = boto3.resource('s3',)
+    s3.Bucket(bucketname).upload_file(sourcepath, logname)
